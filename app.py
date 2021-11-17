8e95cd07ca367055385bf5dcbb871c0969490aee
J. Myrick <63320517+jmyrick02@users.noreply.github.com>
2021-11-16 19:14:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(port=os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
 
 valid_characters = "0123456789abcdefghijklmnopqrstuvwxyz"
 
